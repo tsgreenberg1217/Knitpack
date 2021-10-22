725b65af3cc19f01aca5498d05f9750837267566
Todd Greenberg <tsgreenberg1217@gmail.com>
2021-10-22 00:32:18 -0400

    use bitmap

--- app/src/main/java/com/tgreenberg/knitpack/activities/CreateActivity.kt
+++ app/src/main/java/com/tgreenberg/knitpack/activities/CreateActivity.kt
@@ -1,25 +1,27 @@
 package com.tgreenberg.knitpack.activities
 
-import KnitPackApi
-import android.net.Uri
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Surface
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.create_page.CreateProjectPage
 import com.example.create_page.KnittingProjectViewModel
 import com.example.knitpacktheme.theme.KnitPackTheme
+import com.tgreenberg.core.bitmapFromUri
 import com.tgreenberg.core.models.KnitUri
+import com.tgreenberg.core.resizeByHeight
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
+
 
 @AndroidEntryPoint
 class CreateActivity : ComponentActivity() {
@@ -28,13 +30,17 @@ class CreateActivity : ComponentActivity() {
     private val knittingProjectViewModel: KnittingProjectViewModel by viewModels()
 
     private val imageLauncher = registerForActivityResult(ActivityResultContracts.GetContent()) {
-        knittingProjectViewModel.setKnitProjectImage(KnitUri(it))
+        try {
+            it.bitmapFromUri(contentResolver)
+                .resizeByHeight(350)
+                .run { knittingProjectViewModel.setKnitProjectImage(KnitUri(this)) }
+        } catch (e: Exception) {
+        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val coroutine = rememberCoroutineScope()
             KnitPackTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize()
@@ -46,9 +52,7 @@ class CreateActivity : ComponentActivity() {
                             imageLauncher.launch(it)
                         },
                         submitProject = {
-                            coroutine.launch {
-//                                knittingProjectViewModel.submitProject()
-                            }
+//                            knittingProjectViewModel.submitProject()
                         })
                 }
 
@@ -58,9 +62,3 @@ class CreateActivity : ComponentActivity() {
 
 
 }
-
-
-//@Preview
-//@Composable fun checkitout(){
-//    KnitFormUI.KnitPictures()
-//}
